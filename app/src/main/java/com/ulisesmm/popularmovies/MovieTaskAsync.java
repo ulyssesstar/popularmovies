9e1052ea3aeffb1c90e46cd4639a5a4ea38cfc2a
ulyssesstar <odiseo.r@gmail.com>
2017-05-28 12:38:53 -0500

    Add files via upload

--- app/src/main/java/com/ulisesmm/popularmovies/MovieTaskAsync.java
+++ app/src/main/java/com/ulisesmm/popularmovies/MovieTaskAsync.java
@@ -4,10 +4,13 @@ import com.ulisesmm.popularmovies.util.Movie;
 
 import java.util.ArrayList;
 
-/**
- * Created by ulises on 28/05/17.
- */
 
+/**
+ * Created by Adrian Ulises Mercado Martínez on 26/05/17.
+ *
+ * This code is based on student and sunshine examples
+ * of the course Associate Android Developer Fast Track Nanodegree Program of Udacity
+ */
 public interface MovieTaskAsync {
     public void movieFinishedTask(ArrayList<Movie> movies);
 }
